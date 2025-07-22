3645aa7b6033a452a28f65fbf4b602421b5d2035
Manuel-latorre <manuel.latorre11@gmail.com>
2025-07-21 22:49:39 -0300

    Implement global validation pipe and Swagger documentation setup in main.ts; remove unused AppController from app.module.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
@@ -7,7 +6,7 @@ import { PlansModule } from './modules/plans/plans.module';
 
 @Module({
   imports: [AuthModule, UsersModule, PlansModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
