b25f3aac28fbef6c752d2bf8b59180350d0200a3
Manuel-latorre <manuel.latorre11@gmail.com>
2025-08-09 11:41:12 -0300

    Enable CORS in main.ts for specified origins and update API documentation title and description to reflect Fitapp branding.

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Fitapp API')
+    .setDescription('The Fitapp API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('fitapp')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
